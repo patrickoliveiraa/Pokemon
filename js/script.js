77c4ef14193b1fafbb3faea7101c0a8522edefc6
patrickoliveiraa <patrickolsi@hotmail.com>
2023-10-30 22:04:23 -0300

    não finalizado

--- js/script.js
+++ js/script.js
@@ -1,27 +1,14 @@
-function buscaPokemon(){
-    
-document.getElementById("buscarPokemon").addEventListener("click", function(){
-    const nomePokemon = "nomePokemon";//Substitua pelo nome do Pokemon desejado
-const url=`https://pokeapi.co/api/v2/pokemon/${nomePokemon}`;
-XMLHttpRequest
-var xhr= new XMLHttpRequest();
-xhr.open('GET', url, true);
-xhr.onload=function() { 
-        if (xhr.status === 200) {
-            const data = JSON.parse(xhr.responseText);
-            exibirDadosPokemon(data);
-        } else {
-            alert("Erro ao buscar o Pokemon. Verifique o nome e tente novamente.");
-        }
-    };
-    xhr.send();
-});
+function buscaPokemon() {
+    const nomePokemon = document.getElementById('nomePokemon').value
+    const url = `https://pokeapi.co/api/v2/pokemon/${nomePokemon}`;
 
-function exibirDadosPokemon(pokemon) {
-    document.getElementById("nomePokemon").textContent = pokemon.name;
-    const outrosDetalhes = document.getElementById("outrosDetalhes");
-    outrosDetalhes.innerHTML = "";
-    outrosDetalhes.innerHTML += `<li>Altura: ${pokemon.height} decímetros</li>`;
-    outrosDetalhes.innerHTML += `<li>Peso: ${pokemon.weight} hectogramas</li>`;
-    // Você pode adicionar mais detalhes aqui, dependendo do que deseja exibir.
-}}
+    // Solicitação GET.
+    fetch(url)
+        // Tratamento do sucesso
+        .then(response => response.json())  // converter para json
+        .then(json => {
+            document.getElementById('nome').value = json.name
+            document.getElementById('especie').value = json.species
+        })    //imprimir dados no console
+        .catch(err => console.log('Erro de solicitação', err)); // lidar com os erros por catch
+}
